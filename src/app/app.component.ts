1768084cd3bc4ad3b88b85d4eb81d66233d15265
flexGURU <mukunajohn329@gmail.com>
2025-04-05 22:53:11 +0300

    fine tuning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
-import { NavComponent } from './student/components/nav/nav.component';
+import { NavComponent } from './shared/nav/nav.component';
 import { ToastModule } from 'primeng/toast';
 
 @Component({
---
+++ src/app/shared/nav/nav.component.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
+import { AuthService } from '../services/auth.service';
+import { Observable } from 'rxjs';
+import { User } from '@supabase/supabase-js';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.css',
+  imports: [MenubarModule, CommonModule, ButtonModule],
+})
+export class NavComponent {
+  authService = inject(AuthService);
+  userName$: Observable<User | null> = this.authService.currentUser$;
+
+  logout = () => {
+    this.authService.signOut().subscribe((res) => {
+      localStorage.clear();
+    });
+  };
+}
